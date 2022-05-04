74506a6f0b191fed0f393d6225f3aad3cc9bc9b1
Yiqi Chen <1900012725@pku.edu.cn>
2022-05-04 20:08:14 +0800

    routing algorithm BAM implemented. This algorithm is similar to RPM, but it considers congestion, which I implement randomly here

--- compiler/route_algorithms/mesh.py
+++ compiler/route_algorithms/mesh.py
@@ -2,6 +2,7 @@ import networkx as nx
 from router import Router
 import copy
 import random
+import numpy as np
 
 class MeshTreeRouter(Router):
 
@@ -242,8 +243,154 @@ class WhirlTreeRouter(Router):
         return tree
 
 
+class BAMTreeRouter(Router):
+
+    def __init__(self, diameter) -> None:
+        super().__init__(diameter)
+
+    
+    def tree_pruner(self, tree, v, p_node, pp_node):
+        neighbors = nx.neighbors(tree, v)
+        if_pruned_all = True
+        if (p_node and pp_node):
+            if (p_node // self.diameter == pp_node // self.diameter) or (p_node % self.diameter == v % self.diameter):
+                if len(list(nx.neighbors(tree, p_node))) == 1 and (not tree.nodes[p_node]['dest']):
+                    tree.remove_edge(pp_node, p_node)
+                    tree.remove_edge(p_node, v)
+                    tree.remove_node(p_node)
+                    tree.add_edge(pp_node, v)
+                    p_node = pp_node
+                    if_pruned_all = False
+
+        for i in list(neighbors):
+            if not self.tree_pruner(tree, i, v, p_node):
+                if_pruned_all = False
+        return if_pruned_all
+
+        
+
+
+    def get_tree(self, tree, node: int, dests: list) -> None:
+        if node in dests:
+            tree.nodes[node]['dest'] = True
+            dests.remove(node)
+        # partition
+        dests_parts = [[] for i in range(8)]
+        dests_di = [[] for i in range(4)] #0,1,2,3 corresponding to N,S,W,E
+        
+        for i in dests:
+            if i % self.diameter == node % self.diameter: #part 1,5
+                if i // self.diameter < node // self.diameter: #part 1. attention, the first node is 0 here
+                    dests_parts[1].append(i)
+                if i // self.diameter > node // self.diameter: #part 5
+                    dests_parts[5].append(i)
+
+            if i % self.diameter < node % self.diameter: #part 2,3,4
+                if i // self.diameter < node // self.diameter: #part 2
+                    dests_parts[2].append(i)
+                if i // self.diameter == node // self.diameter: #part 3
+                    dests_parts[3].append(i)
+                if i // self.diameter > node // self.diameter: #part 4
+                    dests_parts[4].append(i)
+
+            if i % self.diameter > node % self.diameter: #part 0,7,6
+                if i // self.diameter < node // self.diameter: #part 0
+                    dests_parts[0].append(i)
+                if i // self.diameter == node // self.diameter: #part 7
+                    dests_parts[7].append(i)
+                if i // self.diameter > node // self.diameter: #part 6
+                    dests_parts[6].append(i)
+        
+        # compulsory direction
+        dests_di[0] += dests_parts[1]
+        dests_di[1] += dests_parts[5]
+        dests_di[2] += dests_parts[3]
+        dests_di[3] += dests_parts[7]
+        
+        #we randomly set the congestion bit
+        N_ne = random.randint(0,1)
+        E_ne = not N_ne
+        N_nw = random.randint(0,1)
+        W_nw = not N_nw
+        S_sw = random.randint(0,1)
+        W_sw = not S_sw
+        S_se = random.randint(0,1)
+        E_se = not S_se
+
+        # rules
+        p = np.zeros(8)
+        n_p = np.zeros(8)
+        for i in range(8):
+            p[i] = len(dests_parts[i])
+            n_p[i] = not p[i]
+        N_p0 = p[1]*n_p[7] + p[1]*p[7]*N_ne + n_p[1]*n_p[7]*N_ne
+        E_p0 = n_p[1]*p[7] + p[1]*p[7]*E_ne + n_p[1]*n_p[7]*E_ne
+        N_p2 = p[1]*n_p[3] + p[1]*p[3]*N_nw + n_p[1]*n_p[3]*N_nw
+        W_p2 = n_p[1]*p[3] + p[1]*p[3]*W_nw + n_p[1]*n_p[7]*W_nw
+        S_p4 = p[5]*n_p[3] + p[5]*p[3]*S_sw + n_p[5]*n_p[3]*S_sw
+        W_p4 = n_p[5]*p[3] + p[5]*p[3]*W_sw + n_p[5]*n_p[3]*W_sw
+        S_p6 = p[5]*n_p[7] + p[5]*p[7]*S_se + n_p[5]*n_p[7]*S_se
+        E_p6 = n_p[5]*p[7] + p[5]*p[7]*E_se + n_p[5]*n_p[7]*E_se
+
+        if N_p0:
+            dests_di[0] += dests_parts[0]
+        else:
+            dests_di[3] += dests_parts[0]
+
+        if N_p2:
+            dests_di[0] += dests_parts[2]
+        else:
+            dests_di[2] += dests_parts[2]
+
+        if S_p4:
+            dests_di[1] += dests_parts[4]
+        else:
+            dests_di[2] += dests_parts[4]
+
+        if S_p6:
+            dests_di[1] += dests_parts[6]
+        else:
+            dests_di[3] += dests_parts[6]
+
+        #add nodes
+        if dests_di[0]:
+            tree.add_node(node-self.diameter, root=False)
+            tree.nodes[node-self.diameter]['dest'] = False
+            tree.add_edge(node, node-self.diameter)
+            self.get_tree(tree, node-self.diameter, dests_di[0])
+        
+        if dests_di[1]:
+            tree.add_node(node+self.diameter, root=False)
+            tree.nodes[node+self.diameter]['dest'] = False
+            tree.add_edge(node, node+self.diameter)
+            self.get_tree(tree, node+self.diameter, dests_di[1])
+
+        if dests_di[2]:
+            tree.add_node(node-1, root=False)
+            tree.nodes[node-1]['dest'] = False
+            tree.add_edge(node, node-1)
+            self.get_tree(tree, node-1, dests_di[2])
+
+        if dests_di[3]:
+            tree.add_node(node+1, root=False)
+            tree.nodes[node+1]['dest'] = False
+            tree.add_edge(node, node+1)
+            self.get_tree(tree, node+1, dests_di[3])
+
+    
+    def route(self, source: int, dests: list) -> nx.DiGraph:
+        tree = nx.DiGraph()
+        tree.add_node(source, root=True)
+        self.get_tree(tree, source, dests)
+        while not self.tree_pruner(tree, source, None, None):
+            pass
+
+        return tree
+
+
 
 if __name__ == "__main__":
     #router = WhirlTreeRouter(4)
-    router = RPMTreeRouter(4)
+    #router = RPMTreeRouter(4)
+    router = BAMTreeRouter(4)
     print(router.route(9, [0, 2, 3, 13, 15]).edges())
