fcd0271604dec6b62ce1367eeab80f586c40dafe
zjc990112 <zjc990112@gmail.com>
2022-06-26 17:56:39 +0800

    update for DSE

--- compiler/global_control.py
+++ compiler/global_control.py
@@ -39,7 +39,7 @@ search_dataflow = False
 extract_traffic = False
 timeloop_verbose = False
 # Search time
-timeout = 30
+timeout = 5
 # Core numbers (default: number specified in database/arch/arch.yaml)
 top_level_cnt = None
 
